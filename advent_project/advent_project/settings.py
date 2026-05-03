20466fa05f8ed1183897d2ac5d92377e00dea1df
dsineirobarreiro <dsineirobarreiro@gmail.com>
2026-05-03 19:55:24 +0200

    chore: render config

--- advent_project/advent_project/settings.py
+++ advent_project/advent_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#b-@0f8&0i0t5=yc+(+fq$7^m$&0(wa-axy%_73fygol9j^wav'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.1.167', 'localhost', '127.0.0.1']
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -68,8 +69,6 @@ TEMPLATES = [
         },
     },
 ]
-
-STATIC_URL = '/static/'
 
 WSGI_APPLICATION = 'advent_project.wsgi.application'
 
@@ -123,5 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ADVENT_START_DATE = date(2026, 4, 18)
